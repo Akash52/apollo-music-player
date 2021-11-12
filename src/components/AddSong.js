9c16b1df4715a2ae219773227a8c7a83f840300d
Akash52 <ac8572611@gmail.com>
2021-11-12 14:03:50 +0530

    Add song from desing

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,100 @@
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  makeStyles,
+} from '@material-ui/core'
+import { AddBoxOutlined, Link } from '@mui/icons-material'
+import { Button, InputAdornment } from '@mui/material'
 import React from 'react'
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '10',
+  },
+  urlInput: {
+    margin: theme.spacing(1),
+  },
+  addSongButton: {
+    margin: theme.spacing(1),
+  },
+  dialog: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: '90%',
+  },
+}))
+
 const AddSong = () => {
-  return <div>Add Song</div>
+  const classes = useStyles()
+  const [dialog, setDialog] = React.useState(false)
+
+  function handleCloseDialog() {
+    setDialog(false)
+  }
+  return (
+    <div className={classes.container}>
+      <Dialog
+        className={classes.dialog}
+        open={dialog}
+        onClose={handleCloseDialog}
+      >
+        <DialogTitle>Edit Song</DialogTitle>
+
+        <DialogContent>
+          <img
+            src="https://images.unsplash.com/photo-1636640157099-a67767c2cd35?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt="Song Image"
+            className={classes.thumbnail}
+          />
+          <TextField margin="dense" name="title" label="Title" fullWidth />
+          <TextField margin="dense" name="artist" label="Artist" fullWidth />
+          <TextField
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="secondary">
+            Cancel
+          </Button>
+          <Button variant="outlined" color="primary">
+            Add Song
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <TextField
+        className={classes.urlInput}
+        placeholder="Add YouTube or Soundcloud URL"
+        fullWidth
+        margin="normal"
+        type="url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment>
+              <Link />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        className={classes.addSongButton}
+        onClick={() => setDialog(true)}
+        variant="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+      >
+        Add
+      </Button>
+    </div>
+  )
 }
 
 export default AddSong
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@material-ui/core/styles'
-import { grey, indigo } from '@material-ui/core/colors'
+import { grey } from '@material-ui/core/colors'
 
 const theme = createTheme({
   palette: {
